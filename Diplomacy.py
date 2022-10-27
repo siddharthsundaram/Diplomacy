bad9972ed1e07eae0fe617283e6594db47c408a7
sidsun <siddharthsundaram@utexas.edu>
2022-10-26 20:31:14 -0500

    completed fight logic, made diplomacy class, and completed logic part of project

--- Diplomacy.py
+++ Diplomacy.py
@@ -12,6 +12,9 @@ class Army:
     def getLocation(self):
         return self.location
 
+    def setLocation(self, newLocation):
+        self.location = newLocation
+
     def getMovement(self):
         return self.movement
 
@@ -27,8 +30,9 @@ class Army:
         '''elif self.movement=='Support':
             self.support(dict,alt)
             return'''
+        return
 
-    def support(self,dict,recipient):
+    def support(self,dict,name,location,newAction,recipient):
         if recipient in dict:
             dict[recipient].troops+=1
 
@@ -36,70 +40,111 @@ class Army:
         self.location=newLocation
 
     def die(self):
-        if self.troops==0:self.location='[dead]'
+        self.location='[dead]'
 
-def print_dict(d):
-    for key in d:
-        print(d[key])
+class Diplomacy:
+    def __init__(self, lis):
+        self.lis = lis      # list of raw inputs; PREVIOUSLY lis in below functions
+        self.armies = dict()    # dictionary of {armyName: armyObject}; PREVIOUSLY d in below functions
+        self.setup()
 
+
+    def print_dict(self):
+        for key in self.armies:
+            print(self.armies[key])
+
+
+    def setup(self):
+        loc={}
+        for x in range(len(self.lis)):                           #commence movements
+            self.lis[x]=self.lis[x].split()                           #lis[x] is the string input (ex. A Madrid Hold)
+            temp=self.lis[x][0]                                  #lis[x][0] is the name of the army (ex. A), lis[x][1] is the location (ex. Madrid)
+            self.armies[temp]=Army(self.lis[x][0],self.lis[x][1],self.lis[x][2])     #d[temp] is the army object
+            #lis[x].pop(0)                                   #lis[x] now only has location, newAction, and alt
+            #print(d[temp])
+            #if self.armies[temp].getMovement!="Support":
+            self.armies[temp].newMovement(self.armies,*self.lis[x])                  #execute movement
+            loc[temp]=self.armies[temp].getLocation()                 #enter location into loc dict to reference; key=army name, value=location
+            #print(d[temp])
+        
+        #print_dict(loc)
+        #print_dict(d)
+
+        # self.supportTest(d,loc,lis)
+        #print(self.armies)
+        self.supportTest(loc)
+
+    # def supportTest(armies,locations,lis):   #takes a dictionary of armies (key=army name, value=army object) and dictionary of locations (key=army name, value=army location)
+    def supportTest(self, locations):    
+        locs={}                             #locs is a dictionary (key=army location, value=list of army names at that location)
+        for key in locations:
+            if locations[key] not in locs:
+                locs[locations[key]]=[key]
+            else:
+                locs[locations[key]].append(key)
+
+        rem={k:v for k,v in locs.items() if len(locs[k])!=1}    #rem is a dictionary (key=army location, value=list of armies at that location) with only fight areas
+
+        for key in rem:                #change all attacked armies Support to Hold
+            for x in rem[key]:         #x is the name of one army at the location rem[key]
+                if self.armies[x].getMovement()=="Support":
+                    self.armies[x].setMovement("Hold")
+        
+        for x in range(len(self.lis)):
+            temp=self.lis[x][0]
+            if self.armies[temp].getMovement()=="Support":
+                self.armies[temp].support(self.armies,*self.lis[x])    #run all the supports for armies that aren't being attacked
+
+        self.fight(rem)
+        
+        ### insert fight function; compare all locations, if 2+ armies are in same city, make them fight ###
+        ### fight! go through all the remaining lists in locs (armies in one location) and see who (if anyone) wins ###
+
+    def fight(self, rem):
+        winner = False
+        for key in rem:
+            most = self.armies[rem[key][0]].troops
+            for i in rem[key]:
+                if self.armies[i].troops > most:
+                    most = self.armies[i].troops
+            # most = max(i.troops for i in rem[key])
+            countMost = 0
+            for i in rem[key]:
+                if self.armies[i].troops == most: countMost += 1
+            if countMost > 1:
+                for army in rem[key]:
+                    self.armies[army].die()
+                # winner = False
+            else:
+                for army in rem[key]:
+                    # print(army, self.armies[army].troops)
+                    if self.armies[army].troops != most:
+                        self.armies[army].die()
+                # winner = True
+                # winner_index = rem[key].index(most)
+
+            # if winner: rem[key][winner_index].setLocation(key)
+
+
+
+        #print("locs:")
+        #print_dict(locs) #tests where the armies are
+
+        #print("armies:")
+        self.print_dict() #true output 
+    
 def inputs(): #makes a tuple of input strings
     inp=[]              #makes a list with each input line as a new index
     while 1:
         i=input()
         if i=="": break
         inp.append(i)
-    setup(inp)
-
-def setup(lis):
-    d={}
-    loc={}
-    for x in range(len(lis)):                           #commence movements
-        lis[x]=lis[x].split()                           #lis[x] is the string input (ex. A Madrid Hold)
-        temp=lis[x][0]                                  #lis[x][0] is the name of the army (ex. A), lis[x][1] is the location (ex. Madrid)
-        d[temp]=Army(lis[x][0],lis[x][1],lis[x][2])     #d[temp] is the army object
-        #lis[x].pop(0)                                   #lis[x] now only has location, newAction, and alt
-        #print(d[temp])
-        d[temp].newMovement(d,*lis[x])                  #execute movement
-        loc[temp]=d[temp].getLocation()                 #enter location into loc dict to reference; key=army name, value=location
-        #print(d[temp])
-    
-    #print_dict(loc)
-    #print_dict(d)
-
-    supportTest(d,loc,lis)
-
-def supportTest(armies,locations,lis):   #takes a dictionary of armies (key=army name, value=army object) and dictionary of locations (key=army name, value=army location)
-    locs={}                             #locs is a dictionary (key=army location, value=list of army names at that location)
-    for key in locations:
-        if locations[key] not in locs:
-            locs[locations[key]]=[key]
-        else:
-            locs[locations[key]].append(key)
-
-    rem={k:v for k,v in locs.items() if len(locs[k])!=1}    #rem is a dictionary (key=army location, value=list of armies at that location) with only fight areas
-
-    for key in rem:                #change all attacked armies Support to Hold
-        for x in rem[key]:         #x is the name of one army at the location rem[key]
-            if armies[x].getMovement()=="Support":
-                armies[x].setMovement("Hold")
-    
-    for x in range(len(lis)):
-        temp=lis[x][0]
-        if armies[temp].getMovement()=="Support":
-            armies[temp].newMovement(armies,*lis[x])    #run all the supports for armies that aren't being attacked
-
-    ### insert fight function; compare all locations, if 2+ armies are in same city, make them fight ###
-    ### fight! go through all the remaining lists in locs (armies in one location) and see who (if anyone) wins ###
-
-    #print("locs:")
-    #print_dict(locs) #tests where the armies are
-
-    #print("armies:")
-    print_dict(armies) #true output    
-    
+    # setup(inp)
+    return inp
 
 def main():
-    inputs()
+    game = Diplomacy(inputs())
+
 
 if __name__ == "__main__":
     main()
