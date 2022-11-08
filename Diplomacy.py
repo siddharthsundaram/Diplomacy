6508f3bab508f6623201741e64648765d7be4a2b
Lauren Adams <laurensmadams@utexas.edu>
2022-11-07 22:39:36 -0600

    cleaned up Diplomacy.py

--- Diplomacy.py
+++ Diplomacy.py
@@ -9,7 +9,6 @@ class Army:
 
     def __str__(self):
         return self.name+' '+str(self.location)
-        #return self.name+' '+str(self.location)+' '+str(self.troops)
 
     def getLocation(self):
         return self.location
@@ -21,14 +20,10 @@ class Army:
         self.movement=newMovement
 
     def newMovement(self,dict,name,location,newAction,alt=None):
-        #(self,dict,alt=None):
         if self.movement=='Hold':return
         elif self.movement=='Move':
             self.move(alt)
             return
-        '''elif self.movement=='Support':
-            self.support(dict,alt)
-            return'''
         return
 
     def support(self,dict,name,location,newAction,recipient):
@@ -43,8 +38,8 @@ class Army:
 
 class Diplomacy:
     def __init__(self, lis):
-        self.lis = lis      # list of raw inputs; PREVIOUSLY lis in below functions
-        self.armies = dict()    # dictionary of {armyName: armyObject}; PREVIOUSLY d in below functions
+        self.lis = lis              # list of raw inputs
+        self.armies = dict()        # dictionary of {armyName: armyObject}
         self.res = None
         self.setup()
 
@@ -61,18 +56,17 @@ class Diplomacy:
 
     def setup(self):
         loc={}
-        for x in range(len(self.lis)-1):                           #commence movements
-            self.lis[x]=self.lis[x].split()                           #lis[x] is the string input (ex. A Madrid Hold)
-            temp=self.lis[x][0]                                  #lis[x][0] is the name of the army (ex. A), lis[x][1] is the location (ex. Madrid)
+        for x in range(len(self.lis)-1):                                             #commence movements
+            self.lis[x]=self.lis[x].split()                                          #lis[x] is the string input (ex. A Madrid Hold)
+            assert len(self.lis[x])==3 or len(self.lis[x])==4                        #check precondition for inputs
+            temp=self.lis[x][0]                                                      #lis[x][0] is the name of the army (ex. A), lis[x][1] is the location (ex. Madrid)
             self.armies[temp]=Army(self.lis[x][0],self.lis[x][1],self.lis[x][2])     #d[temp] is the army object
             self.armies[temp].newMovement(self.armies,*self.lis[x])                  #execute movement
-            loc[temp]=self.armies[temp].getLocation()                 #enter location into loc dict to reference; key=army name, value=location
-        
+            loc[temp]=self.armies[temp].getLocation()                                #enter location into loc dict to reference; key=army name, value=location
         self.supportTest(loc)
 
-    # def supportTest(armies,locations,lis):   #takes a dictionary of armies (key=army name, value=army object) and dictionary of locations (key=army name, value=army location)
-    def supportTest(self, locations):    
-        locs={}                             #locs is a dictionary (key=army location, value=list of army names at that location)
+    def supportTest(self, locations):                           #tests if any army movements are supporting another army    
+        locs={}                                                 #locs is a dictionary (key=army location, value=list of army names at that location)
         for key in locations:
             if locations[key] not in locs:
                 locs[locations[key]]=[key]
@@ -81,15 +75,15 @@ class Diplomacy:
 
         rem={k:v for k,v in locs.items() if len(locs[k])!=1}    #rem is a dictionary (key=army location, value=list of armies at that location) with only fight areas
 
-        for key in rem:                #change all attacked armies Support to Hold
-            for x in rem[key]:         #x is the name of one army at the location rem[key]
+        for key in rem:                                         #change all attacked armies Support to Hold
+            for x in rem[key]:                                  #x is the name of one army at the location rem[key]
                 if self.armies[x].getMovement()=="Support":
                     self.armies[x].setMovement("Hold")
         
         for x in range(len(self.lis)-1):
             temp=self.lis[x][0]
-            if self.armies[temp].getMovement()=="Support":
-                self.armies[temp].support(self.armies,*self.lis[x])    #run all the supports for armies that aren't being attacked
+            if self.armies[temp].getMovement()=="Support":      #checks and runs all the supports for armies that aren't being attacked
+                self.armies[temp].support(self.armies,*self.lis[x])
 
         self.fight(rem)
         
@@ -111,63 +105,23 @@ class Diplomacy:
                     if self.armies[army].troops != most:
                         self.armies[army].die()
 
-        self.res = self.print_dict() #true output
+        self.res = self.print_dict()
 
 def diplomacy_solve(inputs):
     game = Diplomacy(inputs.split("\n"))
     print(game.res + "\n")
     return game.res + "\n"
 
-def diplomacy_read(s):
-    """
-    read a string of armies and actions
-    s a string
-    return a list split by army and corresponding action
-    """
-
+def diplomacy_read(s):                 # reads a string s of armies and actions, returns a list split by army and corresponding action
     a = []
     for i in s:
         a.append(str(i))
-    # a = s.split("\n")
-    # a.pop(-1)
     return "".join(a)
 
-def diplomacy_print(w, input, output):
-    """
-    print three ints
-    w a writer
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    v the max cycle length
-    """
+def diplomacy_print(w, input, output):  # formats output
     w.write(str(input) + str(output))
 
-def diplomacy_eval(r, w):
-    """
-    r a reader
-    w a writer
-    """
-    # print(r)
+def diplomacy_eval(r, w):               # r is a reader, w is a writer
     inp = diplomacy_read(r)
-    # print(inp)
     out = str(diplomacy_solve(inp))
-    # print(out)
     diplomacy_print(w, inp, out)
-
-
-if __name__ == "__main__":
-    diplomacy_solve("A London Hold\nB Dallas Move London\nC Lima Support B\nD Austin Move Lima\nE Shanghai Support C\n")
-    r = StringIO(("A Madrid Hold\nB Barcelona Move Madrid\nC London Support B\n"))
-    # print(diplomacy_read(r))
-    w = StringIO()
-    # diplomacy_print(w, "A Madrid Hold\nB Barcelona Move Madrid\nC London Support B\n", "A [dead]\nB Madrid\nC London\n")
-    # diplomacy_eval(r, w)
-    # w.getvalue()
-
-# move all the armies first
-# maybe have a list of all the existing locations
-# go through list of locations and check if there is more than 1 army in location(s)
-# for the locations w more than 1 army, check if any of the armies are supporting, and change to hold
-# for each army in a location, add the supports and find army with max support
-# check if max support is in the list more than once, if so every army dies
-# if max support only occurs once, army with max support wins and occupies location
